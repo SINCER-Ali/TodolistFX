ea5caa2dee6c790c9398b912b98833f7af88d6db
SINCER-Ali <a.sincer@lprs.fr>
2024-09-10 15:48:35 +0200

    arboréssance et formulaire inscription

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module appli.todolistjx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens appli to javafx.fxml;
+    exports appli;
+    exports appli.acceuil;
+    opens appli.acceuil to javafx.fxml;
+}
