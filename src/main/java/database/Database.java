a5b98f6433784f8a1f9b2d3286adacbe1b42c1d7
SINCER-Ali <a.sincer@lprs.fr>
2024-10-01 15:26:15 +0200

    avancement cours 2

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -1,12 +1,34 @@
 package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Database {
     private String serveur = "localhost";
 
-    private String introJavaFx = "introJavaFx";
+    private String nomDeLaBase = "intro_javafx";
 
     private String utilisateur = "root";
 
     private String MotDePasse = "";
+
+
+    public String getUrl() {
+        return "jdbc:mysql://"+serveur+"/"+nomDeLaBase+"?serverTimezone=UTC";
+    }
+    public Connection getConnection() {
+        try {
+            Connection cnx = DriverManager.getConnection(this.getUrl(), this.utilisateur, this.MotDePasse);
+            System.out.print("État de la connexion :");
+            System.out.print(cnx.isClosed() ? "fermée" : "ouverte \r\n");
+            return cnx;
+        } catch (SQLException e) {
+            System.out.print("Erreur lors de la tentative de connexion à la base de données");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
+
+
 
---
+++ src/main/java/model/UtilisateurControlleur.java
@@ -0,0 +1,61 @@
+package model;
+
+public class UtilisateurControlleur {
+    private int idUser;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String mdp;
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+
+    @Override
+    public String toString() {
+        return "UtilisateurControlleur{" +
+                "idUser=" + idUser +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", mdp='" + mdp + '\'' +
+                '}';
+    }
+}
